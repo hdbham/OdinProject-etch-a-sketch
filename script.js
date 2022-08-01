942c7be62be83af8c1ee51a7af7b84a477b0568f
Hunter <hunter.d.burningham@gmail.com>
2022-07-31 23:36:49 -0600

    todo

--- script.js
+++ script.js
@@ -2,29 +2,46 @@
 generateGrid(); //run
 onMouseHover(); 
 
+
+
+let color;
+
+
 function generateGrid(){
 var n=10;//take grid column value as you want
 var count = 0;
 var gridContent= '';
-
 for(var i = 0; i < n; i++) {
     gridContent +='<div class="row">';
     for(j = 0; j < n; j++) {
-        gridContent+='<div class="gridsquare" id='  + (++count) + '></div>';
+        gridContent+='<div class="gridsquare" tabindex='  + (++count) + '></div>';
     }
     gridContent+="</div>";
 }
 document.getElementById("gridContent").innerHTML = gridContent; 
 }
 
+// function handlekeys(ev){
+//   down 40: +n
+//   up 38: -n
+//   left 29: -1
+//   right 37: +1
+// }
 
 //queryselector w. event listener for all elements of a class
 function onMouseHover(){
 document.querySelectorAll('.gridsquare').forEach(item => {
   item.addEventListener('mouseover', event => {
- 	  event.target.style.background = "black";
+ 	  event.target.style.background = color;
+       console.log(document.activeElement.tabIndex);
+
   })
 })
+    document.getElementById('my-select').addEventListener('change', function() {
+    console.log('You selected: ', this.value);
+    return color = this.value;
+  });
+
 }
 
 
